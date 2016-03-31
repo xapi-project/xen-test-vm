f1d18fbdca7e77f7ad51c3840c00d1b0bfee6732
Christian Lindig <lindig@citrix.com>
2016-03-31 12:32:34 +0100

    Fixed build dependencies.

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 
+opam pin add -n mirage-xen git://github.com/jonludlam/mirage-platform#reenable-suspend-resume
 
-opam install mirage-types-lwt 
-opam install mirage-xen-minios 
+opam pin add -n mirage-bootvar-xen git://github.com/jonludlam/mirage-bootvar-xen#better-parser
 
-opam pin add mirage-xen git://github.com/jonludlam/mirage-platform#reenable-suspend-resume
-
-opam pin add mirage-bootvar-xen git://github.com/jonludlam/mirage-bootvar-xen#better-parser
-
-opam pin add minios-xen git://github.com/jonludlam/mini-os#suspend-resume3
+opam pin add -n minios-xen git://github.com/jonludlam/mini-os#suspend-resume3
 
 
+opam install mirage-xen
+opem install mirage-console
+opem install mirage-bootvar-xen
+opem install mirage
