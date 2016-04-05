374f24ea9e56517dacd54edafa0e5cbc05b0ec0a
Christian Lindig <lindig@citrix.com>
2016-04-05 17:12:05 +0100

    Some restructuring before introducing a side channel

--- src/mirage_guest_agent.ml
+++ src/mirage_guest_agent.ml
@@ -8,10 +8,20 @@ module Main (C: V1_LWT.CONSOLE) = struct
   let log   c fmt = Printf.kprintf (fun msg -> C.log   c msg) fmt 
   let log_s c fmt = Printf.kprintf (fun msg -> C.log_s c msg) fmt
 
-  let suspend c =
+  (* implementation of control operations *)
+  let suspend client c =
+    OS.Xs.(immediate client (fun h -> rm h "control/shutdown")) >>= fun _ -> 
     OS.Sched.suspend () >>= fun cancelled -> 
-    log c "cancelled=%d" cancelled;
-    return cancelled
+    log_s c "cancelled=%d" cancelled >>= fun () ->
+    log_s c "About to read domid"    >>= fun () ->
+    OS.Xs.(immediate client (fun h -> read h "domid")) >>= fun domid -> 
+    log_s c "We're back: domid=%s" domid >>= fun _ -> 
+    return true
+
+  let poweroff ()   = OS.Sched.shutdown OS.Sched.Poweroff; return false 
+  let reboot ()     = OS.Sched.shutdown OS.Sched.Reboot;   return false 
+  let halt ()       = OS.Sched.shutdown OS.Sched.Poweroff; return false
+  let crash ()      = OS.Sched.shutdown OS.Sched.Crash;    return false
 
     (* Documentation 
      * http://mirage.github.io/mirage-xen/#Xs
@@ -19,39 +29,28 @@ module Main (C: V1_LWT.CONSOLE) = struct
      * http://mirage.github.io/mirage-types/#V1:CONSOLE
      *)
 
+  (* event loop *)  
   let start c = 
-    log_s c "xs_watch ()" >>= fun () -> 
     OS.Xs.make () >>= fun client -> 
     let rec loop () = 
       OS.Xs.(immediate client (fun h -> directory h "control")) >>= fun dir -> 
-      begin if List.mem "shutdown" dir then begin
-          OS.Xs.(immediate client (fun h -> read h "control/shutdown")) >>= fun msg ->
-          log_s c "Got control message: %s" msg >>= fun () ->
-          match msg with
-          | "suspend" -> 
-            OS.Xs.(immediate client (fun h -> rm h "control/shutdown")) >>= fun _ -> 
-            suspend c >>= fun _ -> 
-            log_s c "About to read domid" >>= fun _ ->
-            OS.Xs.(immediate client (fun h -> read h "domid")) >>= fun domid -> 
-            log_s c "We're back: domid=%s" domid >>= fun _ -> 
-            return true
-          | "poweroff" -> 
-            OS.Sched.shutdown OS.Sched.Poweroff;
-            return false (* Doesn't get here! *)
-          | "reboot" ->
-            OS.Sched.shutdown OS.Sched.Reboot;
-            return false (* Doesn't get here! *)
-          | "halt" ->
-            OS.Sched.shutdown OS.Sched.Poweroff;
-            return false
-          | "crash" ->
-            OS.Sched.shutdown OS.Sched.Crash;
-            return false
-          | _ -> 
-            return false
-        end else return false end >>= fun _ ->
+      if not @@ List.mem "shutdown" dir then 
+        return false 
+      else begin
+        OS.Xs.(immediate client (fun h -> read h "control/shutdown")) >>= fun msg ->
+        log_s c "Got control message: %s" msg >>= fun () ->
+        ( match msg with
+        | "suspend"   -> suspend client c
+        | "poweroff"  -> poweroff ()
+        | "reboot"    -> reboot ()
+        | "halt"      -> halt ()
+        | "crash"     -> crash ()
+        | _           -> return false
+        )
+      end >>= fun _ ->
       OS.Time.sleep 1.0 >>= fun _ ->
       loop ()
-    in loop ()
+    in 
+      loop ()
 
 end
