5b75c541db65957d2f791899b8bda1cdd8ccaf7c
Christian Lindig <lindig@citrix.com>
2016-04-07 14:56:22 +0100

    The VM now reacts to control/testing even without msg in control/shutdown.
    Added documentation in the README: The kernel reads control messages
    from the Xen Store from "control/shutdown" and responds to them. In
    addition, it reads from "control/testing". Here is how the two interact:
    
      shutdown  testing
      -----------------
      msg                 -> regular case, responds to msg
      msg       tst       -> ignores msg, responds to tst
                tst       -> responds to tst

--- README.md
+++ README.md
@@ -35,7 +35,32 @@ As root on $HOST:
     
 Once installed, use the CLI on the host to operate the VM or XenCenter.
 
-# Debugging
+# Out-of-Band Control Messages
+
+The kernel reads control messages from the Xen Store from
+"control/shutdown" and responds to them. In addition, it reads from 
+"control/testing". Here is how the two interact:
+
+  shutdown  testing
+  -----------------
+  msg                 -> regular case, responds to msg
+  msg       tst       -> ignores msg, responds to tst
+            tst       -> responds to tst
+
+Valid messages are:
+
+  suspend  
+  poweroff 
+  reboot   
+  halt     
+  crash    
+
+Typically, control/shutdown is written only by Xen. To write to
+control/testing:
+
+  xenstore write /local/domain/<domid>/control/testing reboot
+
+# Debugging the VM
 
 To direct console output of the VM to a file, you can tell the $HOST:
 
--- src/mirage_guest_agent.ml
+++ src/mirage_guest_agent.ml
@@ -1,13 +1,13 @@
 (* This code implements a minimal kernel that responds to
  * life cycle events.
- *)
+*)
 
 (* Documentation of important interfaces:
  * http://mirage.github.io/mirage-xen/#Xs
  * http://mirage.github.io/mirage-xen/#Sched
  * http://mirage.github.io/mirage-types/#V1:CONSOLE
  * http://mirage.github.io/xenstore/#Xs_protocol
- *)
+*)
 
 
 module Main (C: V1_LWT.CONSOLE) = struct
@@ -22,7 +22,7 @@ module Main (C: V1_LWT.CONSOLE) = struct
 
   (* These are like [C.log] and [C.log_s] but accept printf-style
    * formatting instructions.
-   *)
+  *)
   let log_s c fmt = Printf.kprintf (fun msg -> C.log_s c msg) fmt
   (* let log   c fmt = Printf.kprintf (fun msg -> C.log   c msg) fmt *)
 
@@ -32,21 +32,21 @@ module Main (C: V1_LWT.CONSOLE) = struct
   (* [read_opt client path] reads [path] from the Xen Store and
    * returns it as an option value on success, and [None] otherwise.
    * Unexpected errors still raise an exception.
-   *)
+  *)
   let read_opt client  path  = 
     Lwt.catch
       ( fun () ->
-        read client path >>= fun msg -> 
-        return (Some msg)
+          read client path >>= fun msg -> 
+          return (Some msg)
       )
       ( function 
-      | Xs_protocol.Enoent _ -> return None 
-      | ex                   -> Lwt.fail ex 
+        | Xs_protocol.Enoent _ -> return None 
+        | ex                   -> Lwt.fail ex 
       )
 
   (* The suspend operation acknowledges the request by removing 
    * "control/shutdown" from Xen Store.
-   *)
+  *)
   let suspend client c =
     rm client control_shutdown>>= fun () ->
     OS.Sched.suspend ()                 >>= fun cancelled -> 
@@ -62,6 +62,7 @@ module Main (C: V1_LWT.CONSOLE) = struct
   let halt ()       = OS.Sched.(shutdown Poweroff); return false
   let crash ()      = OS.Sched.(shutdown Crash);    return false
 
+  (** [dispatch] implements the reaction to control messages *)
   let dispatch client c = function
     | "suspend"  -> suspend client c
     | "poweroff" -> poweroff ()
@@ -69,8 +70,11 @@ module Main (C: V1_LWT.CONSOLE) = struct
     | "halt"     -> halt ()
     | "crash"    -> crash ()
     | msg        -> log_s c "Unknown message %s" msg >>= fun () -> 
-                    return false
+      return false
 
+  (** [override client c msg tst] implements the reaction to
+   * having received [msg] but reacting as having received [tst] instead
+  *)
   let override client c msg tst = 
     log_s c "overriding command %s with %s" msg tst >>= fun () ->
     rm client control_testing >>= fun () ->
@@ -81,17 +85,20 @@ module Main (C: V1_LWT.CONSOLE) = struct
     OS.Xs.make ()               >>= fun client -> 
     read client "domid"         >>= fun domid ->
     log_s c "domid=%s" domid    >>= fun () ->
-    let rec loop () = 
+    let rec loop tick  = 
       read_opt client control_shutdown >>= fun msg ->
       read_opt client control_testing  >>= fun tst ->
       ( match msg, tst with
-      | None, _         ->  log_s c "%s is empty" control_shutdown >>= fun () ->
-                            return false
-      | Some msg, None      -> dispatch client c msg
-      | Some msg, Some tst  -> override client c msg tst
+        | None, None ->  
+          log_s c "%s for %s is empty %d" control_shutdown domid tick >>= fun () ->
+          return false
+        | Some msg, None      -> dispatch client c msg (* regular case *) 
+        | None    , Some tst  -> override client c "none" tst
+        | Some msg, Some tst  -> override client c msg tst
+        | Some msg, Some tst  -> override client c msg tst
       ) >>= fun _ ->
       sleep 1.0 >>= fun _ ->
-      loop ()
+      loop (tick+1)
     in 
-      loop ()
+    loop 0
 end
