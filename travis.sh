37de8a89469f50c4bde175b385d3faea7cacc938
Christian Lindig <lindig@gmail.com>
2016-04-11 21:22:58 +0100

    Fixing more build dependencies

--- travis.sh
+++ travis.sh
@@ -4,6 +4,7 @@
 sudo add-apt-repository ppa:avsm/ppa
 sudo apt-get update
 sudo apt-get install -y ocaml ocaml-native-compilers opam
+sudo apt-get install -y ocamlbuild ocamlfind
 
 ocamlc -version
 opam -version
