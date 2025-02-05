4c906ccd7f79d516432b4095180729b21d2e6283
Christian Lindig <christian.lindig@cloud.com>
2025-02-05 16:41:44 +0000

    Cleanup: don't rely on Docker; build in a switch
    * Remove all auto-generated code and re-generate it
    * Provide install.sh to install kernel on a hos
    
    Signed-off-by: Christian Lindig <christian.lindig@cloud.com>

--- Makefile
+++ Makefile
@@ -3,28 +3,19 @@
 
 PACKAGE = 	xen-test-vm
 PREFIX = 	.
-LIB = 		$(PREFIX)/$(PACKAGE)/lib
 VM = 		./src/dist/xenserver-test-vm.xen
 
 all:		src
-		$(MAKE) -C src/ all
-		ls -lh $(VM)
+		cd src; mirage configure -t xen
+		$(MAKE) -C src/
 
-package: 	src
-		opam pin add -y xen-test-vm .
-		opam install xen-test-vm
-
-install: 	
-		mkdir -p $(LIB)
-		cp $(VM) $(LIB)
-
-remove: 	
-		rm -f $(LIB)/$(VM)
-		
 clean:
-		$(MAKE) -C src clean
+		$(MAKE) -C src/ clean
 
-.PHONY: 	all clean install release
+distclean:
+		git clean -fdx
+
+.PHONY: 	all clean distclean
 
 
 
--- install.sh
+++ install.sh
@@ -1,24 +1,40 @@
-#! /bin/bash
+#!/usr/bin/env bash
 #
-# Download and install the xen-test-vm on a XenServer.
+# Install xen-test-vm on a XenServer.
 #
-# usage: install.sh [name]
+# usage: install.sh host
 #
 
-set -ex
+VM=./src/dist/xenserver-test-vm.xen
 
-NAME=${1:-xen-test-vm}
+set -o errexit
+set -o pipefail
 
-VERSION="0.1.17"
-GH="https://github.com/xapi-projext"
-VM="$GH/xen-test-vm/releases/download/$VERSION/test-vm.xen.gz"
-KERNEL="xen-test-vm-${VERSION//./-}.xen.gz"
-GUEST="/boot/guest"
+if [[ $# != 1 ]]; then
+   cat <<EOF 
+Usage: $0 host
 
+Install $VM on host; this requires SSH access to the host.
+EOF
+  exit 1
+fi
 
-mkdir -p "$GUEST"
-curl --fail -s -L "$VM" > "$GUEST/$KERNEL" || rm -f "$GUEST/$KERNEL"
-UUID=$(xe vm-create name-label="$NAME")
-xe vm-param-set PV-kernel="$GUEST/$KERNEL" uuid="$UUID"
+if ! test -f "$VM"; then
+  echo "$VM not found; run make to build it"
+  exit 1
+fi
 
-
+host="$1"
+scp "$VM" "$host:/tmp"
+ssh "root@$host" <<-'EOF'
+  guest="/var/lib/xcp/guest"
+  gzip /tmp/xenserver-test-vm.xen
+  mkdir -p "$guest" && mv /tmp/xenserver-test-vm.xen.gz "$guest"
+  UUID=$(xe vm-create name-label="minion-$$")
+  xe vm-param-set PV-kernel="$guest/xenserver-test-vm.xen.gz" uuid="$UUID"
+  xe vm-param-set uuid=$UUID domain-type=pvh
+  xe vm-param-add uuid=$UUID param-name=platform pae=true nx=true
+  xe vm-param-set uuid=$UUID PV-kernel=$guest/xenserver-test-vm.xen.gz
+  xe vm-memory-limits-set uuid=$UUID dynamic-max=16777216 \
+    dynamic-min=16777216 static-max=16777216 static-min=16777216
+EOF
